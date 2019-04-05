0e32588707f1fe9d623197075f3372be53a02472
Unknown <losbanjitos@gmail.com>
2019-04-05 23:38:53 +0200

    Add UI elements, countdown, players, chars

---
+++ Assets/Scripts/CharacterMod.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMod : MonoBehaviour
+{
+
+	[SerializeField] Text Character;
+	[SerializeField] bool Sandman;
+	
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		if (Sandman)
+		{
+			Character.text = "Sandman";
+		}
+		else
+		{
+			Character.text = "Pyromaniac";
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Sandman)
+		{
+			Character.text = "Sandman";
+		}
+		else
+		{
+			Character.text = "Pyromaniac";
+		}
+	}
+}
---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+	int CuttedTime = 0;
+	[SerializeField] Text CountdownText;
+
+	float CurrentMainTime = 0f;
+	[SerializeField] float StartingMainTime = 10f;
+
+	
+	float CurrentSupportTime = 0f;
+	[SerializeField] float StartingSupportTime = 10f;
+
+	[SerializeField] int textSize = 40;
+
+
+	[SerializeField] int round = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		CurrentMainTime = StartingMainTime;
+		CurrentSupportTime = StartingSupportTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		string print = "";
+
+		if (CurrentMainTime>0)
+		{
+			textSize = 40;
+			CurrentMainTime -= 1 * Time.deltaTime;
+			if(CurrentMainTime > 0)
+			{
+				CuttedTime = (int)CurrentMainTime;
+				print = CuttedTime.ToString();
+			}
+			else
+			{
+				round++;
+				CurrentSupportTime = StartingSupportTime;
+			}
+
+		}
+		else
+		{
+			textSize = 20;
+			CurrentSupportTime -= 1 * Time.deltaTime;
+			if (CurrentSupportTime > 0)
+			{
+				CuttedTime = (int)CurrentSupportTime;
+				print = "next Round \n in "+ CuttedTime;
+			}
+			else
+			{
+				CurrentMainTime = StartingMainTime;
+			}
+			
+				
+	
+		
+		}
+		CountdownText.text = print;
+		CountdownText.fontSize = textSize;
+
+
+	}
+}
