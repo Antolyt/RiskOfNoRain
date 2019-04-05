9e750f8406c5893864e906d94c24af8db2ebf853
Matthis Hagen <matthis.hagen@o2online.de>
2019-04-05 23:19:23 +0200

    Add Pickups and Statbonuses

---
+++ Assets/Scripts/Pickups/PickupManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupManager : MonoBehaviour
+{
+    [SerializeField]
+    float pickupSpawnTime;
+    float pickupSpawnTimer;
+
+    [SerializeField]
+    List<Pickup> pickupList;
+
+    [SerializeField]
+    List<PickupSpawner> spawnLocations;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickupSpawnTimer = pickupSpawnTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pickupSpawnTimer -= Time.deltaTime;
+        if (pickupSpawnTimer <= 0)
+        {
+            pickupSpawnTimer = pickupSpawnTime;
+            SpawnRandomPickup();
+        }
+    }
+
+    /// <summary>
+    /// Spawns a random pickup at a random location.
+    /// </summary>
+    void SpawnRandomPickup()
+    {
+        List<PickupSpawner> freeSpawnLocations = spawnLocations.FindAll(p => p.HasActivePickup == false);
+
+        if (spawnLocations != null)
+        {
+            PickupSpawner spawnLocation = freeSpawnLocations[Random.Range(0, freeSpawnLocations.Count)];
+
+            Pickup pickup = Instantiate(pickupList[Random.Range(0, pickupList.Count)], spawnLocation.transform);
+        }
+    }
+}
