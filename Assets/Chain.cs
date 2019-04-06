a91fab3fbd0bf5ed1030dd3aded6e500bce3cb53
Tony Schwensfeier <finalcow@yahoo.de>
2019-04-06 15:57:18 +0200

    Update Player.prefab Added Chain

---
+++ Assets/Chain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chain : MonoBehaviour
+{
+    public LineRenderer lr;
+    public Transform start;
+    public Transform end;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lr.SetPosition(0, start.position);
+        lr.SetPosition(1, end.position);
+    }
+}
