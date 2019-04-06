39e189b1ef8d76c253378f6a16f79320e89a50c4
Unknown <losbanjitos@gmail.com>
2019-04-06 03:23:00 +0200

    add pickup ui

---
+++ Assets/Scripts/UI/PickupList.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupList : MonoBehaviour
+{
+
+	[SerializeField] List<Pickup> pickups;
+	[SerializeField] List<Pickup> uipickups;
+	[SerializeField] [Range (0, 10)] float rotationSpeed = 1f;
+	
+    void Start()
+    {
+		for (int i =0; i<pickups.Count; i++)
+		{
+			Vector3 offset = new Vector3(0, i * 2, 0);
+
+			uipickups.Add(Instantiate(pickups[i], this.transform.position - offset ,Quaternion.identity, this.transform));
+
+
+		}
+	}
+
+    void Update()
+    {
+		foreach(Pickup p in uipickups)
+		{
+			p.transform.RotateAround(Vector3.up, rotationSpeed * Time.deltaTime);
+		}
+		
+
+        
+    }
+}
